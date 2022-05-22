062c6b9b27b5f73d6ae76ff104ba4640549ec9a5
Joseph Graham <joseph@xylon.me.uk>
2022-05-22 08:32:05 +0100

    initial commit

---
+++ test/clj/xyloobservations/handler_test.clj
@@ -0,0 +1,27 @@
+(ns xyloobservations.handler-test
+  (:require
+    [clojure.test :refer :all]
+    [ring.mock.request :refer :all]
+    [xyloobservations.handler :refer :all]
+    [xyloobservations.middleware.formats :as formats]
+    [muuntaja.core :as m]
+    [mount.core :as mount]))
+
+(defn parse-json [body]
+  (m/decode formats/instance "application/json" body))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (mount/start #'xyloobservations.config/env
+                 #'xyloobservations.handler/app-routes)
+    (f)))
+
+(deftest test-app
+  (testing "main route"
+    (let [response ((app) (request :get "/"))]
+      (is (= 200 (:status response)))))
+
+  (testing "not-found route"
+    (let [response ((app) (request :get "/invalid"))]
+      (is (= 404 (:status response))))))

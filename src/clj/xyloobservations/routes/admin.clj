d3fef56991d12a2c545401e407b17fe2e85cbe93
Joseph Graham <joseph@xylon.me.uk>
2022-06-18 19:08:12 +0100

    Make admin pages accessible from title-bar if logged-in.

--- src/clj/xyloobservations/routes/admin.clj
+++ src/clj/xyloobservations/routes/admin.clj
@@ -12,46 +12,57 @@
   `(hash-map ~@(mapcat (juxt keyword identity) xs)))
 
 (defn add-tag-page [request]
-  (layout/render request "add_tag.html" {}))
+  (layout/render request "add_tag.html" {:loggedin (contains? (request :session) :user)}))
 
 (defn add-tag-submit [request]
   (let [{:keys [tagname description]} (request :params)]
     ;; we add it and show the form again so they
     ;; may add another
     (try (do (admin/add-tag! tagname description)
-             (layout/render request "add_tag.html" {:msgtype "success" :msgtxt "successfully added tag"}))
-         (catch AssertionError e (layout/render request "add_tag.html" {:msgtype "error" :msgtxt (str "validation error: " (.getMessage e))})))
-    ))
+             (layout/render request "add_tag.html" {:msgtype "success"
+                                                    :msgtxt "successfully added tag"
+                                                    :loggedin (contains? (request :session) :user)}))
+         (catch AssertionError e
+           (layout/render request "add_tag.html" {:msgtype "error"
+                                                  :msgtxt (str "validation error: " (.getMessage e))
+                                                  :loggedin (contains? (request :session) :user)})))))
 
 (defn upload-image-page [request]
-  (layout/render request "upload_image.html"))
+  (layout/render request "upload_image.html" {:loggedin (contains? (request :session) :user)}))
 
 (defn upload-image-submit [request]
   (let [file (request :multipart-params)
         caption (-> request :params :caption)]
     (try (do (admin/upload-image! file caption)
-             (layout/render request "upload_image.html" {:msgtype "success" :msgtxt "successfully uploaded image"}))
-         (catch AssertionError e (layout/render request "upload_image.html" {:msgtype "error" :msgtxt (str "validation error: " (.getMessage e))})))))
+             (layout/render request "upload_image.html" {:msgtype "success"
+                                                         :msgtxt "successfully uploaded image"
+                                                         :loggedin (contains? (request :session) :user)}))
+         (catch AssertionError e
+           (layout/render request "upload_image.html" {:msgtype "error"
+                                                       :msgtxt (str "validation error: " (.getMessage e))
+                                                       :loggedin (contains? (request :session) :user)})))))
 
 (defn image-settings-page [request]
   (let [image_id (Integer/parseInt ((request :query-params) "id"))
         attached_tags (db/tag_names_of_image {:image_id image_id})
         caption ((db/get-caption {:image_id image_id}) :caption)
-        all_tags (db/all_tags)]
-    (layout/render request "image_settings.html" (map-of image_id attached_tags all_tags caption))))
+        all_tags (db/all_tags)
+        loggedin (contains? (request :session) :user)]
+    (layout/render request "image_settings.html" (map-of image_id attached_tags all_tags caption loggedin))))
 
 (defn image-settings-submit [request]
   (let [image_id (Integer/parseInt ((request :query-params) "id"))
         newtag (-> request :params :tag Integer/parseInt)
         dropdownsubmit (-> request :params :dropdownsubmit)
-        newcaption (-> request :params :caption)]
+        newcaption (-> request :params :caption)
+        loggedin (contains? (request :session) :user)]
     (if (= dropdownsubmit "true")
      (admin/tag-image! newtag, image_id)
       (admin/update-caption! newcaption, image_id))
     (let [attached_tags (db/tag_names_of_image {:image_id image_id})
           caption ((db/get-caption {:image_id image_id}) :caption)
           all_tags (db/all_tags)]
-      (layout/render request "image_settings.html" (map-of image_id attached_tags all_tags caption)))))
+      (layout/render request "image_settings.html" (map-of image_id attached_tags all_tags caption loggedin)))))
 
 (defn admin-routes []
   [""
--- src/clj/xyloobservations/routes/home.clj
+++ src/clj/xyloobservations/routes/home.clj
@@ -18,9 +18,11 @@
     (if (> (count tags) 0)
       (layout/render request "gallery.html" {:images (homefunc/matching-images tags)
                                              :filters (db/names-for-tags {:tags tags})
-                                             :alltags (db/all-tags-with-images)})
+                                             :alltags (db/all-tags-with-images)
+                                             :loggedin (contains? (request :session) :user)})
       (layout/render request "gallery.html" {:images (homefunc/images-with-tags)
-                                             :alltags (db/all-tags-with-images)}))))
+                                             :alltags (db/all-tags-with-images)
+                                             :loggedin (contains? (request :session) :user)}))))
 
 (defn image [request]
   (let [image_id (Integer/parseInt ((request :query-params) "id"))

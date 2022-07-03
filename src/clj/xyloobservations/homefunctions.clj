ddcfdbee795391dfadf95a96ccceeeaf2afbffb0
Joseph Graham <joseph@xylon.me.uk>
2022-07-03 10:55:41 +0100

    remove apparently unused function

--- src/clj/xyloobservations/homefunctions.clj
+++ src/clj/xyloobservations/homefunctions.clj
@@ -4,11 +4,6 @@
    [xyloobservations.db.core :as db]
    [clojure.java.io :as io]))
 
-(defn add-images [tag]
-  "given a map of values for the tag, add a extra key containing a vector of maps of imageids and captions"
-  (let [tag_id (tag :tag_id)]
-    (assoc tag :images (db/images-by-tag {:tag_ref tag_id}))))
-
 (defn fetch-image [image_id]
   "just run the fetch-image query and convert the imagedata into an io/input-stream"
   (let [{:keys [imagedata mimetype]} (db/fetch-image {:image_id image_id})]

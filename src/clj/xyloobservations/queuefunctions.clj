9f832b5fe0ddebc977ea017604041ecc371aa124
Joseph Graham <joseph@xylon.me.uk>
2022-07-17 18:24:39 +0100

    re-factor the code to be a bit more readable

--- src/clj/xyloobservations/queuefunctions.clj
+++ src/clj/xyloobservations/queuefunctions.clj
@@ -74,49 +74,47 @@
 (defn extract-key [buildme innermap]
   (conj buildme {(keyword (innermap :identifier)) (dissoc innermap :filepath :identifier)}))
 
+(defn update-and-save
+  "upload the items from the uploadme var and save the metadata"
+  [image_id object_ref uploadme]
+  (case (env :image-store)
+    "s3"
+    (upload-to-s3 object_ref uploadme)
+    "filesystem"
+    (save-to-filesystem object_ref uploadme))
+  (db/save-meta! {:imagemeta (generate-string (reduce extract-key {} uploadme))
+                  :image_id image_id}))
+
 (defn message-handler
   [ch meta ^bytes payload]
   (let [message (nippy/thaw payload)
         {:keys [image_id object_ref mimetype size imagebytes]} message]
-    (log/info (format "received image id %s with ref %s"
-                      image_id
-                      object_ref))
+    (log/info (format "received image id %s with ref %s" image_id object_ref))
     (db/update-progress! {:image_id image_id :progress "resizing"})
 
     ;; resize
     (try
-      (do (def uploadme (resizers/resize size imagebytes image_id mimetype))
-          (log/info (format "resized image %s successfully" image_id))
-          (db/update-progress! {:image_id image_id :progress "saving"}))
+      (def uploadme (resizers/resize size imagebytes image_id mimetype))
+      (log/info (format "resized image %s successfully" image_id))
+      (db/update-progress! {:image_id image_id :progress "saving"})
       (catch Exception e
         ;; we log the output of the exception, then we throw it again
         ;; to stop any further execution
         (log/info (format "failed resizing image %s with exception: %s" image_id e))
         (db/update-progress! {:image_id image_id :progress "failed resizing"})
-        (throw (ex-info e
-                        {:type :resize-exception}))))
+        (throw (ex-info e {:type :resize-exception}))))
 
     ;; upload the items from the "uploadme" var and save the metadata
     (try
-      (do
-        (case (env :image-store)
-          "s3"
-          (upload-to-s3 object_ref uploadme)
-          "filesystem"
-          (save-to-filesystem object_ref uploadme))
-        (db/save-meta! {:imagemeta (generate-string (reduce extract-key {} uploadme))
-                        :image_id image_id})
-        (log/info (format "uploaded image id %s with ref %s"
-                          image_id
-                          object_ref))
-        (db/update-progress! {:image_id image_id :progress "complete"}))
+      (update-and-save image_id object_ref uploadme)
+      (log/info (format "uploaded image id %s with ref %s" image_id object_ref))
+      (db/update-progress! {:image_id image_id :progress "complete"})
       (catch Exception e
         ;; we log the output of the exception, then we throw it again
         ;; to stop any further execution
         (log/info (format "failed saving image %s with exception: %s" image_id e))
         (db/update-progress! {:image_id image_id :progress "failed saving"})
-        (throw (ex-info e
-                        {:type :save-exception}))))
+        (throw (ex-info e {:type :save-exception}))))
 
     ;; delete the temporary files resizers/resize made earlier
     (cleanup-files uploadme)))
